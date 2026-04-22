a8508cc55f4418f4c4c04fdc873ceaec6d4ddc46
Casey Labs <4674433+CaseyLabs@users.noreply.github.com>
2026-04-22 14:37:46 -0700

    Generalize static asset URL test hosts

--- internal/web/template_func_test.go
+++ internal/web/template_func_test.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"crypto/tls"
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -9,28 +10,33 @@ import (
 func TestStaticAssetURL(t *testing.T) {
 	t.Parallel()
 
-	httpReq, err := http.NewRequest(http.MethodGet, "http://gomud.net/", nil)
+	const (
+		requestHost = "request.example"
+		cdnHost     = "cdn.example"
+	)
+
+	httpReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s/", requestHost), nil)
 	if err != nil {
 		t.Fatalf("http.NewRequest() error = %v", err)
 	}
 
-	httpsReq, err := http.NewRequest(http.MethodGet, "https://gomud.net/", nil)
+	httpsReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("https://%s/", requestHost), nil)
 	if err != nil {
 		t.Fatalf("http.NewRequest() error = %v", err)
 	}
 	httpsReq.TLS = &tls.ConnectionState{}
 
-	proxiedHTTPSReq, err := http.NewRequest(http.MethodGet, "http://gomud.net/", nil)
+	proxiedHTTPSReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s/", requestHost), nil)
 	if err != nil {
 		t.Fatalf("http.NewRequest() error = %v", err)
 	}
 	proxiedHTTPSReq.Header.Set("X-Forwarded-Proto", "https")
 
-	forwardedHTTPSReq, err := http.NewRequest(http.MethodGet, "http://gomud.net/", nil)
+	forwardedHTTPSReq, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://%s/", requestHost), nil)
 	if err != nil {
 		t.Fatalf("http.NewRequest() error = %v", err)
 	}
-	forwardedHTTPSReq.Header.Add("Forwarded", `for=203.0.113.8;proto=https;host=gomud.net`)
+	forwardedHTTPSReq.Header.Add("Forwarded", fmt.Sprintf(`for=203.0.113.8;proto=https;host=%s`, requestHost))
 
 	tests := []struct {
 		name      string
@@ -49,37 +55,37 @@ func TestStaticAssetURL(t *testing.T) {
 		{
 			name:      "http request keeps http cdn",
 			req:       httpReq,
-			cdnBase:   "http://files.gomud.net",
+			cdnBase:   fmt.Sprintf("http://%s", cdnHost),
 			assetPath: "/static/css/gomud.css",
-			want:      "http://files.gomud.net/static/css/gomud.css",
+			want:      fmt.Sprintf("http://%s/static/css/gomud.css", cdnHost),
 		},
 		{
 			name:      "https request drops insecure cdn",
 			req:       httpsReq,
-			cdnBase:   "http://files.gomud.net",
+			cdnBase:   fmt.Sprintf("http://%s", cdnHost),
 			assetPath: "/static/css/gomud.css",
 			want:      "/static/css/gomud.css",
 		},
 		{
 			name:      "proxied https request drops insecure cdn",
 			req:       proxiedHTTPSReq,
-			cdnBase:   "http://files.gomud.net",
+			cdnBase:   fmt.Sprintf("http://%s", cdnHost),
 			assetPath: "/static/css/gomud.css",
 			want:      "/static/css/gomud.css",
 		},
 		{
 			name:      "forwarded https request drops insecure cdn",
 			req:       forwardedHTTPSReq,
-			cdnBase:   "http://files.gomud.net",
+			cdnBase:   fmt.Sprintf("http://%s", cdnHost),
 			assetPath: "/static/css/gomud.css",
 			want:      "/static/css/gomud.css",
 		},
 		{
 			name:      "https request keeps secure cdn",
 			req:       httpsReq,
-			cdnBase:   "https://cdn.example.com/",
+			cdnBase:   fmt.Sprintf("https://%s/", cdnHost),
 			assetPath: "static/js/webclient-core.js",
-			want:      "https://cdn.example.com/static/js/webclient-core.js",
+			want:      fmt.Sprintf("https://%s/static/js/webclient-core.js", cdnHost),
 		},
 		{
 			name:      "invalid cdn falls back local",
