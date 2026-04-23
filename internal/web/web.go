9d2bc798ff12fffa8b03e1ad32b5fdc32c8863e0
Casey Labs <4674433+CaseyLabs@users.noreply.github.com>
2026-04-23 10:17:38 -0700

    fix go linting

--- internal/web/web.go
+++ internal/web/web.go
@@ -444,10 +444,10 @@ func serveTemplate(w http.ResponseWriter, r *http.Request) {
 	}
 
 	templateData := map[string]any{
-		"REQUEST":    r,
-		"PATH":       reqPath,
-		"CONFIG":     configs.GetConfig(),
-		"STATS":      GetStats(),
+		"REQUEST":        r,
+		"PATH":           reqPath,
+		"CONFIG":         configs.GetConfig(),
+		"STATS":          GetStats(),
 		"ASSET_BASE_URL": publicAssetBase(r, configs.GetFilePathsConfig().WebCDNLocation.String()),
 		"NAV": []WebNav{
 			{`Home`, `/`},
