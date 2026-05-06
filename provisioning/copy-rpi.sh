0623840ec425af70a35b5c060530a5d859df7aa5
CaseyLabs <4674433+CaseyLabs@users.noreply.github.com>
2026-05-05 20:32:27 -0700

    ci: harden GitHub Actions workflows (#564)

--- provisioning/copy-rpi.sh
+++ provisioning/copy-rpi.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env sh
 #
 # Helper script to compile/copy binary to test server on raspberry pi
 #
@@ -12,12 +13,16 @@ make build_rpi_zero2w
 
 # Kill the process before overwriting the binary
 echo "Killing process on RaspPi: ${RPI_HOST}"
-ssh ${RPI_HOST} 'sudo pkill mud-server'
+ssh "${RPI_HOST}" 'sudo pkill mud-server'
 
-# Copy the binary over, delete the local binary, run the server again using the script setup ont he server
+# Copy the binary over, delete the local binary, run the server again using the script setup on the server
 echo "Copying bin to RaspPi: ${RPI_HOST}"
-scp ./${RPI_BIN} ${RPI_HOST}:${RPI_HOST_PATH}/mud/${RPI_BIN} && \
+if ! scp "./${RPI_BIN}" "${RPI_HOST}:${RPI_HOST_PATH}/mud/${RPI_BIN}"; then
+	exit 1
+fi
 
 echo "Starting Server on RaspPi: ${RPI_HOST}"
-rm ${RPI_BIN} && \
-ssh ${RPI_HOST} -f 'cd ${RPI_HOST_PATH}; sudo ./startup-run-mud.sh'
+if ! rm "${RPI_BIN}"; then
+	exit 1
+fi
+ssh "${RPI_HOST}" -f "cd ${RPI_HOST_PATH}; sudo ./startup-run-mud.sh"
